9f23f2691ee8b0efa7c16ad2bb4ca00fbf8a5cb4
Adrijan-Petek <adrijangithub@gmail.com>
2026-05-17 01:10:29 +0200

    fix: add dedicated app head verification tags

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="base:app_id" content="68d0be1c1aaf9981934f89a8" />
+      <meta
+        name="talentapp:project_verification"
+        content="6ce2499a4805221f8a498bf2a01a258554515a2b5845adc2dc007c189d607d1222d5a5d7b3ecaa2d7179e16fd9da5a3a37efb316d8ebc42b5653b4b2fcd7c02d"
+      />
+    </>
+  );
+}
