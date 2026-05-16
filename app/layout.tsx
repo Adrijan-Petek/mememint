7d8882ff3b36f362db511fa8da3517379c8267ae
Adrijan-Petek <adrijangithub@gmail.com>
2026-05-17 01:02:47 +0200

    feat: add base app id meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ const sourceCodePro = Source_Code_Pro({
 
 export const metadata: Metadata = {
   other: {
+    "base:app_id": "68d0be1c1aaf9981934f89a8",
     "talentapp:project_verification":
       "6ce2499a4805221f8a498bf2a01a258554515a2b5845adc2dc007c189d607d1222d5a5d7b3ecaa2d7179e16fd9da5a3a37efb316d8ebc42b5653b4b2fcd7c02d",
   },
